4b4a2fd5c3e645664fd51053878f2a7f266014c3
ShamanShaz <admin@dcastack.com>
2022-09-29 19:23:13 -0400

    Updated for 1.3.0

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,12 @@ require("@nomiclabs/hardhat-ethers");
 const config_1 = require("hardhat/config");
 const adapter_1 = require("./adapter");
 const setupSafeDeployer = (signer, safe, serivceUrl) => {
-    config_1.extendEnvironment((env) => {
-        env.network.provider = new adapter_1.SafeProviderAdapter(env.network.provider, signer.connect(env.ethers.provider), safe, serivceUrl);
+    (0, config_1.extendEnvironment)((env) => {
+        const { chainId } = env.network.config;
+        if (!chainId) {
+            throw new Error('The chainId was required in hardhat network config');
+        }
+        env.network.provider = new adapter_1.SafeProviderAdapter(env.network.provider, signer.connect(env.ethers.provider), safe, chainId, serivceUrl);
     });
 };
 exports.setupSafeDeployer = setupSafeDeployer;
