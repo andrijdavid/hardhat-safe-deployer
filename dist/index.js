4a9c38ccb76faf409b8a4cf26225b4e5b4b29c29
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-05-10 13:29:45 +0530

    changed setup to async

--- dist/index.js
+++ dist/index.js
@@ -5,14 +5,14 @@ require("@nomiclabs/hardhat-ethers");
 const config_1 = require("hardhat/config");
 const adapter_1 = require("./adapter");
 const setupSafeDeployer = (payload) => {
-    (0, config_1.extendEnvironment)((env) => {
+    (0, config_1.extendEnvironment)(async (env) => {
         const { safe, serivceUrl } = payload;
         const { chainId } = env.network.config;
         if (!chainId) {
             throw new Error('The chainId was required in hardhat network config');
         }
         if (!payload.signer)
-            payload.signer = env.network.provider.getSigner(0);
+            payload.signer = (await env.ethers.getSigners())[0];
         env.network.provider = new adapter_1.SafeProviderAdapter(env.network.provider, payload.signer.connect(env.ethers.provider), safe, chainId, serivceUrl);
     });
 };
--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,15 @@ import "@nomiclabs/hardhat-ethers";
 import { Signer, Wallet } from "ethers"
 import { extendEnvironment } from "hardhat/config"
 import { SafeProviderAdapter } from "./adapter"
-import { EthereumProvider } from "hardhat/types";
-interface MyProvider extends EthereumProvider {
-  getSigner(index?: number): Signer;
-}
 
 export const setupSafeDeployer = (payload: { signer: Wallet | Signer | undefined, safe: string, serivceUrl: string | undefined }) => {
-  extendEnvironment((env) => {
+  extendEnvironment(async (env) => {
     const { safe, serivceUrl } = payload
     const { chainId } = env.network.config;
     if (!chainId) {
       throw new Error('The chainId was required in hardhat network config');
     }
-    if(!payload.signer) payload.signer = (env.network.provider as MyProvider).getSigner(0)
+    if(!payload.signer) payload.signer = (await env.ethers.getSigners())[0]
     env.network.provider = new SafeProviderAdapter(
       env.network.provider,
       payload.signer.connect(env.ethers.provider),
