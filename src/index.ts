74c01636f67cefbb4ae173b26f579d0107e54189
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-05-10 17:35:37 +0530

    use json rpc signer

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,12 @@ import { SafeProviderAdapter } from "./adapter"
 
 export const setupSafeDeployer = (payload: { signer: Wallet | Signer | undefined, safe: string, serivceUrl: string | undefined }) => {
   extendEnvironment((env) => {
-    let fetchedSigner = undefined;
-    env.ethers.getSigners().then(signers=>{
-      fetchedSigner = signers[0]
-    })
-    while (!fetchedSigner) {
-      // do nothing, just wait
-    }
     const { safe, serivceUrl } = payload
     const { chainId } = env.network.config;
     if (!chainId) {
       throw new Error('The chainId was required in hardhat network config');
     }
-    if(!payload.signer) payload.signer = fetchedSigner
+    if(!payload.signer) payload.signer = env.ethers.provider.getSigner(0)
     else payload.signer =  payload.signer.connect(env.ethers.provider)
     env.network.provider = new SafeProviderAdapter(
       env.network.provider,
