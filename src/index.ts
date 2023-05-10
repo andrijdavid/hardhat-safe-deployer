6153841dc3df7dabefb68b5ae925d2c62e47a4d4
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-05-11 00:28:44 +0530

    removed optional param of signer

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,16 @@ import { Signer, Wallet } from "ethers"
 import { extendEnvironment } from "hardhat/config"
 import { SafeProviderAdapter } from "./adapter"
 
-export const setupSafeDeployer = (payload: { signer: Wallet | Signer | undefined, safe: string, serivceUrl: string | undefined }) => {
+export const setupSafeDeployer = (payload: { signer: Wallet | Signer, safe: string, serivceUrl: string | undefined }) => {
   extendEnvironment((env) => {
     const { safe, serivceUrl } = payload
     const { chainId } = env.network.config;
     if (!chainId) {
       throw new Error('The chainId was required in hardhat network config');
     }
-    if(!payload.signer) payload.signer = env.ethers.provider.getSigner(0)
-    else payload.signer =  payload.signer.connect(env.ethers.provider)
     env.network.provider = new SafeProviderAdapter(
       env.network.provider,
-      payload.signer,
+      payload.signer.connect(env.ethers.provider),
       safe,
       chainId,
       serivceUrl
