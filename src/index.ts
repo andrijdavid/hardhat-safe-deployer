58da3ca6e4a5bc9ffe7ef3057beefad751931d2d
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2023-05-10 14:02:08 +0530

    reverted back

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,17 @@ import { extendEnvironment } from "hardhat/config"
 import { SafeProviderAdapter } from "./adapter"
 
 export const setupSafeDeployer = (payload: { signer: Wallet | Signer | undefined, safe: string, serivceUrl: string | undefined }) => {
-  extendEnvironment((env) => {
+  extendEnvironment(async (env) => {
     const { safe, serivceUrl } = payload
     const { chainId } = env.network.config;
     if (!chainId) {
       throw new Error('The chainId was required in hardhat network config');
     }
-    if(!payload.signer) payload.signer = env.ethers.Wallet.createRandom().connect(env.ethers.provider)
+    if(!payload.signer) payload.signer = (await env.ethers.getSigners())[0]
+    else payload.signer =  payload.signer.connect(env.ethers.provider)
     env.network.provider = new SafeProviderAdapter(
       env.network.provider,
-      payload.signer.connect(env.ethers.provider),
+      payload.signer,
       safe,
       chainId,
       serivceUrl
