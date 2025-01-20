99f7796558ed6723d2571d4ff003c5a8ca0a5fb4
Andrij David <andrijdavid@gmail.com>
2025-01-20 01:38:57 +0100

    Update dependency

--- src/adapter.ts
+++ src/adapter.ts
@@ -52,18 +52,6 @@ export class SafeProviderAdapter implements EthereumProvider {
             return resp.data;
         } catch (error) {
             console.error("Axios POST request failed: ", error);
-            if (error.response) {
-                // Server responded with a status other than 2xx
-                console.error("Response data: ", error.response.data);
-                console.error("Response status: ", error.response.status);
-                console.error("Response headers: ", error.response.headers);
-            } else if (error.request) {
-                // Request was made but no response received
-                console.error("Request data: ", error.request);
-            } else {
-                // Something happened while setting up the request
-                console.error("Error message: ", error.message);
-            }
             throw error; // Re-throw the error after logging it
         }
     }
